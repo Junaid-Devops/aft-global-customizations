a6c4667beb9ddc7f99401c52196cb29188c21a8e
Junaid Majumdar <jmajumdar@agero.com>
2026-06-08 18:41:38 -0400

    adding netskope

--- api_helpers/post-api-helpers.sh
+++ api_helpers/post-api-helpers.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 echo "Executing Post-API Helpers"
-
-#!/bin/bash
 echo "=== Starting Post-API Customization Script ==="
 
-# Execute your clean python automation task directly in the build sequence
-python3 $BASE_PATH/api_helpers/python/netskope_enroll.py
+# Using $DEFAULT_PATH/$CUSTOMIZATION accurately maps to your repo root folder in AFT
+python3 $DEFAULT_PATH/$CUSTOMIZATION/api_helpers/python/netskope_enroll.py
 
 echo "=== Completed Post-API Customization Script ==="
