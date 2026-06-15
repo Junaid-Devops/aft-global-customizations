9b922bf6c289105d98f7e1f52a836dff8ff759d8
Junaid-Devops <107377867+Junaid-Devops@users.noreply.github.com>
2026-06-15 15:51:49 -0400

    Update post-api-helpers.sh

--- api_helpers/post-api-helpers.sh
+++ api_helpers/post-api-helpers.sh
@@ -6,4 +6,13 @@ unset AWS_PROFILE
 # Using $DEFAULT_PATH/$CUSTOMIZATION accurately maps to your repo root folder in AFT
 python3 $DEFAULT_PATH/$CUSTOMIZATION/api_helpers/python/netskope_enroll.py
 
+echo "Creating dynamic, isolated SSM parameter for Account: $VENDED_ACCOUNT_ID"
+
+aws ssm put-parameter \
+  --name "/aft/accounts/${VENDED_ACCOUNT_ID}/active" \
+  --value "$VENDED_ACCOUNT_ID" \
+  --type "String" \
+  --overwrite \
+  --description "Isolating runtime variable for TFC execution loop"
+
 echo "=== Completed Post-API Customization Script ==="
