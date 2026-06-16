5789e86c71c33f8b1efff3a91ce9ec4a6acdb38a
Junaid Majumdar <jmajumdar@agero.com>
2026-06-16 11:50:57 -0400

    adding ssm parameter

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,7 +1,16 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_ssm_parameter" "AWSAccount" {
-  name  = "/aws/AWSAccount"
+  name  = "/aft/AWSAccount"
   type  = "String"
   value = data.aws_caller_identity.current.account_id
+}
+
+# This queries AWS for the exact credentials currently being used
+data "aws_caller_identity" "current" {}
+
+# This will print the active IAM Role ARN in your TFC/CodeBuild logs
+output "current_terraform_iam_identity" {
+  value       = data.aws_caller_identity.current.arn
+  description = "The exact IAM Role or User executing this Terraform plan"
 }
