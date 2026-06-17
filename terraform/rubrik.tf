8bfcbd7299b03e266f5e2a9bce7cae3fe25c8639
Junaid Majumdar <jmajumdar@agero.com>
2026-06-17 13:00:48 -0400

    adding rubrik

--- terraform/rubrik.tf
+++ terraform/rubrik.tf
@@ -5,8 +5,7 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      #version = ">= 5.26.0"      
+      source = "hashicorp/aws"
     }
     polaris = {
       source  = "rubrikinc/polaris"
@@ -16,8 +15,7 @@ terraform {
 }
 
 # ====================================================================
- ====================================================================
-# 3. POLARIS CONFIGURATION & MODULE
+# 2. POLARIS CONFIGURATION & MODULE
 # ====================================================================
 data "aws_ssm_parameter" "target_id" {
   name = "/aft/AWSAccountID"
@@ -46,13 +44,13 @@ module "cloud_native" {
 
   aws_account_id   = data.aws_ssm_parameter.target_id.value
   aws_account_name = data.aws_ssm_parameter.target_name.value
-  aws_regions      = ["us-west-2", "us-east-1","us-east-2"]
+  aws_regions      = ["us-west-2", "us-east-1", "us-east-2"]
 
   rsc_aws_features = [
-    { name = "CLOUD_NATIVE_ARCHIVAL",       permission_groups = [] },
-    { name = "CLOUD_NATIVE_PROTECTION",     permission_groups = [] },
-    { name = "CLOUD_NATIVE_S3_PROTECTION",  permission_groups = [] },
+    { name = "CLOUD_NATIVE_ARCHIVAL",            permission_groups = [] },
+    { name = "CLOUD_NATIVE_PROTECTION",          permission_groups = [] },
+    { name = "CLOUD_NATIVE_S3_PROTECTION",       permission_groups = [] },
     { name = "CLOUD_NATIVE_DYNAMODB_PROTECTION", permission_groups = [] },
-    { name = "RDS_PROTECTION",              permission_groups = [] }
+    { name = "RDS_PROTECTION",                   permission_groups = [] }
   ]
 }
